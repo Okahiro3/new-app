095a468ceb1b924629552b22c9c5f8e9a4caa3b4
Okahiro3 <okazaki1995@icloud.com>
2020-07-02 14:35:42 +0900

    インクリメンタルサーチ

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,14 @@
 class UsersController < ApplicationController
 
+def index
+  return nil if params[:keyword] == ""
+  @users = User.where(['name LIKE ?', "%#{params[:keyword]}%"] ).ehere.not(id: current_user.id).limit(10)
+  respond_to do |format|
+    format.html
+    format.json
+  end
+end
+
 def edit
 end
  
